60a313a9197456f7320db2f3909cd7b4f7ae2668
rinsan1120 <keiz5963@gmail.com>
2026-09-09 10:45:30 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { setWorkerUrl } from 'maplibre-gl';
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './styles.css';
 import App from './App';
+
+setWorkerUrl(workerUrl);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
